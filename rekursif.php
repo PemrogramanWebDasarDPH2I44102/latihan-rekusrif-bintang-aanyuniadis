ea063c451851c96744ded33d2fa79c85b0b804ff
aanyuniadis <42663017+aanyuniadis@users.noreply.github.com>
2018-09-13 09:00:56 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,39 @@
 <?php
-
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+echo " Rekursif Piramida Bintang <br>";
+$tampung= 5;
+function rekursif($a,$b){
+	if ($a >0){
+		if($b >0){
+			echo "*";
+			$b--;
+			rekursif($a, $b);
+		}else{
+			echo "<br>";
+			$a--;
+			rekursif($a,$a);
+		}
+	}
 }
-rekursif(5);
+rekursif($tampung,$tampung);
+echo "<br>";
+echo "<br>";
+echo "*/=====================================================*/<br>";
+
+echo " Rekursif Piramida Angka <br>";
+$print= 5;
+function number($x,$y,$print){
+	if ($x <= $print){
+		if($y <= $x){
+			echo "$y";
+			$y++;
+			number($x, $y, $print);
+		}else{
+			echo "<br>";
+			$x++;
+			number($x,1,$print);
+		}
+	}
+}
+number(1,1,$print);
+
 ?>
